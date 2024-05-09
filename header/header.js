c9aaddf5c847716b836c89261e812f013c1f8658
ema <respositorio50@gmail.com>
2024-05-09 13:04:28 -0600

    header y footer por carpeta

--- header/header.js
+++ header/header.js
@@ -13,8 +13,8 @@ vinculo.innerHTML=`
     <div class="envio1"><img src="https://www.clipartmax.com/png/full/269-2697089_pickup-delivery-camion-de-envios-blanco-png.png" alt=""></div>
 
     <div class="envio2">
-        <span class="envio3"></span> 
-        <span class="envio4"></span>
+        <span class="envio3">Envios</span> 
+        <span class="envio4">A Toda Guatemala</span>
     </div>
 
     </div>
@@ -28,9 +28,23 @@ vinculo.innerHTML=`
         <input  type="text" placeholder="" class="barra_busqueda"> <div class="icon_busqueda"></div>
         </div>
 
+    <!-- ----------------Mi cuenta---------------- -->
+    <div class="mi_cuenta">     
+    
+    <div class="cuenta_persona">    <img src="https://images.vexels.me/media/users/3/136558/isolated/preview/43cc80b4c098e43a988c535eaba42c53-person-user-icon.png" alt="">   </div>
+
+    <div class="micuenta_info">
+        <span class="mi_cuenta1">Hola, Estuardo</span> 
+        <span class="mi_cuenta2">Mi cuenta ^ </span>
+    </div>
+    </div>
+
     <!-- -------------------------------- -->
-    <div class="mi_cuenta"></div>
-    <div class="Lista_deseos"></div>
+
+
+    <div class="lista_deseos">     
+    <img src="https://cdn-icons-png.freepik.com/512/744/744635.png" alt="">
+    </div>
 
     <div class="carrito"></div>
 
